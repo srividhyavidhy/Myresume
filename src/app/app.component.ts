aa1613aff02d8983101cad64f1d72f0d3d1509e7
srividhya <srividhyav44@gmail.com>
2023-12-31 13:46:42 +0530

    resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, OnDestroy, } from '@angular/core';
+import { jsPDF } from 'jspdf';
+import domtoimage from 'dom-to-image';
+import html2canvas from 'html2canvas';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'resume';
+  @ViewChild('htmlData') htmlData!: ElementRef;
+  constructor() {}
+  public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    });
+  }
+
+
+ 
+
 }
